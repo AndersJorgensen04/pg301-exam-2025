cdebbc4378d32f33d614bdef74ecf6c4384bcaf3
Anders Jørgensen <Anjo088@student.kristiania.no>
2025-11-08 20:43:18 +0000

    fix some syntax

--- infra-s3/main.tf
+++ infra-s3/main.tf
@@ -1,29 +1,29 @@
 resource "aws_s3_bucket" "sentimentanalyse" {
-    bucket = var.bucket_name
-    
- 
+  bucket = var.bucket_name
+
+
 }
 resource "aws_s3_bucket_lifecycle_configuration" "bucket-lifecycle" {
-    bucket = aws_s3_bucket.sentimentanalyse.id
-     rule {
-        id = "move-temp-file"
-        filter {
-            prefix = "midlertidig/"
-        }
-        transition {
-          days = 365
-          storage_class = "GLACIER_IR"
-        }
-        status = "Enabled"
+  bucket = aws_s3_bucket.sentimentanalyse.id
+  rule {
+    id = "move-temp-file"
+    filter {
+      prefix = "midlertidig/"
     }
-    rule {
-      id = "delete-temp-file"
-      filter {
-        prefix = "midlertidig/"
-      }
-      expiration {
-        days = 365
-      }
-      status = "Enabled"
+    transition {
+      days          = 365
+      storage_class = "GLACIER_IR"
     }
+    status = "Enabled"
+  }
+  rule {
+    id = "delete-temp-file"
+    filter {
+      prefix = "midlertidig/"
+    }
+    expiration {
+      days = 365
+    }
+    status = "Enabled"
+  }
 }
--- infra-s3/output.tf
+++ infra-s3/output.tf
@@ -1,12 +1,12 @@
 output "bucket_name" {
   description = "Name of the s3 sentimentanalyse bucket"
-  value = aws_s3_bucket.sentimentanalyse.bucket
+  value       = aws_s3_bucket.sentimentanalyse.bucket
 }
 output "bucket_arn" {
   description = "sentimentanalyse buckey ARN"
-  value = aws_s3_bucket.sentimentanalyse.arn
+  value       = aws_s3_bucket.sentimentanalyse.arn
 }
 output "region" {
   description = "Region where the bucket is located"
-  value = var.region
+  value       = var.region
 }
